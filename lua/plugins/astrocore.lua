41b677a433d51af32734f48afe782c5e53ecd62f
amandin <123622161+amawdiin@users.noreply.github.com>
2024-05-25 14:23:20 +0600

    amandin suggestions

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,9 +1,15 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
+
+local function switch_terminal_mode()
+  if vim.bo.buftype == "terminal" then
+    if vim.fn.mode() == "n" then
+      vim.cmd "startinsert"
+    else
+      vim.cmd "stopinsert"
+    end
+  end
+end
 
 ---@type LazySpec
 return {
@@ -47,19 +53,18 @@ return {
     -- Mappings can be configured through AstroCore as well.
     -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
-      -- first key is the mode
       n = {
-        -- second key is the lefthand side of the map
+        ["<S-Space>"] = { function() switch_terminal_mode() end, desc = "Switch terminal mode" },
 
         -- navigate buffer tabs with `H` and `L`
-        -- L = {
-        --   function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-        --   desc = "Next buffer",
-        -- },
-        -- H = {
-        --   function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-        --   desc = "Previous buffer",
-        -- },
+        L = {
+          function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+          desc = "Next buffer",
+        },
+        H = {
+          function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+          desc = "Previous buffer",
+        },
 
         -- mappings seen under group name "Buffer"
         ["<Leader>bD"] = {
@@ -77,6 +82,7 @@ return {
         ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
       },
       i = {
+        ["<C-BS>"] = { "<C-w>", desc = "Delete previous word" },
         ["<C-v>"] = { "<cmd> :normal! P <CR>", desc = "Paste" },
         ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
       },
@@ -85,8 +91,7 @@ return {
         [">"] = { ">gv" },
       },
       t = {
-        -- setting a mapping to false will disable it
-        -- ["<esc>"] = false,
+        ["<S-Space>"] = { function() switch_terminal_mode() end, desc = "Switch terminal mode" },
       },
     },
   },
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Mason plugins
-
 ---@type LazySpec
 return {
   -- use mason-lspconfig to configure LSP installations
@@ -23,8 +19,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "prettier",
-        "stylua",
+        -- "prettier",
+        -- "stylua",
         -- add more arguments for adding more null-ls sources
       })
     end,
@@ -35,7 +31,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "python",
+        -- "python",
         -- add more arguments for adding more debuggers
       })
     end,
